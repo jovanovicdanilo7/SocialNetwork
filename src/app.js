c63b1d318e9a0364eceb314524cc84ecacb8af79
jovanovic.danilo7_local <jovanovic.danilo06@gmail.com>
2024-12-08 18:40:20 +0100

    Init commit

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get("/hexa.html", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/Main/hexa.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
