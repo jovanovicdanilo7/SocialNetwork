ab26e7f5d95820f30c5a97ceee61046ec791c9f2
jovanovic.danilo7_local <jovanovic.danilo06@gmail.com>
2024-12-08 17:43:02 +0100

    Local setup done.

--- src/routes/config.js
+++ src/routes/config.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ apiUrl: process.env.API_BASE_URL + "/api" });
+    res.json({ apiUrl: process.env.API_BASE_URL + process.env.PORT + "/api" });
 });
 
 module.exports = router;
